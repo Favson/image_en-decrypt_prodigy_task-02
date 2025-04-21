b29a2f8757c0f1437fdbf78dbaec1898318a4403
Adebisi Favour <adebisimomoluwa@gmail.com>
2025-04-21 16:40:37 +0100

    task 2 with prodigy

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
 root = Tk()
 root.geometry("200x160")  # fixed the × symbol
@@ -10,7 +10,13 @@ def encrypt_image():
         # print(file1)
         file_name=file1.name
         # print(file_name)
-        key =entry1.get(1.0, END)
+        key =entry1.get(1.0, END).strip()
+        
+        if not key.isdigit():
+            messagebox.showerror("Invalid Key", "Please enter a valid numeric key.")
+            return
+        key = int(key)
+        
         print(file_name, key)
         fi = open(file_name, 'rb')
         image=fi.read()
@@ -19,12 +25,14 @@ def encrypt_image():
         
         for index,values in enumerate(image):
             image[index]=values^int(key)
+        messagebox.showinfo("Success", "Successfully Done!")
         fi1 =open(file_name, 'wb')
         fi1.write(image)
         fi1.close()
 
-b1 = Button(root, text="Encrypt", command=encrypt_image)
+b1 = Button(root, text="Encrypt/Decrypt", command=encrypt_image)
 b1.place(x=70, y=10)
+
 
 entry1 = Text(root, height=1, width=10)
 entry1.place(x=50, y=50)  # fixed the capital X
